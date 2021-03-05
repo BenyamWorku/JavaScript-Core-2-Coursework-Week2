8c96293e0cc0fd4b56c3ccb6d2542c51924c796f
BenyamWorku <benworku@gmail.com>
2021-03-05 17:04:50 +0000

    exercise B

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,11 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayofGroceries) {
+  let content = document.querySelector("#content");
+  let listEl = document.createElement("ul");
+  content.appendChild(listEl);
+  for (let item of arrayofGroceries) {
+    listEl.appendChild(document.createElement("li")).textContent=item;
+  }
+
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
