601a0af63eeffbc5afcffb7c33dd63ab2893bba3
BenyamWorku <benworku@gmail.com>
2021-03-06 17:17:19 +0000

    exercise D

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,34 @@
 function todoList(todos) {
-  // Write your code here...
+  let content = document.getElementById("content");
+  let unorderedList = content.appendChild(document
+    .createElement("ul"));
+
+  for (let aTask of todos) {
+    let listEl = document.createElement("li");
+    // listEl.classList.add("classShoppingList");
+    unorderedList.appendChild(listEl).textContent = aTask.todo;
+  }
+  
+
+  let strikeToggler = function (e) {
+    if (e.target.style.textDecoration === "line-through") {
+      e.target.style.textDecoration = "none";
+    }
+    else {
+      e.target.style.textDecoration = "line-through";
+    }
+
+  };
+  unorderedList.addEventListener("click", strikeToggler);
+  
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" }
 ];
 
 todoList(todos);
+console.log(content);
