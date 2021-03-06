fcee4c6381ffa5b212ae4593d4029a00e80d9d91
BenyamWorku <benworku@gmail.com>
2021-03-06 23:31:46 +0000

    exercise E

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,7 +1,23 @@
 function listOfColours(colours) {
-  // Write your code here...
-}
+  let content = document.querySelector("#content");
+  let selectEl = document.createElement("select");
+  content.appendChild(selectEl);
+  let paraEl = document.createElement("p");
+  content.appendChild(paraEl);
+  for (let aColour of colours) {
+    let optionEl = document.createElement("option")
+    selectEl.appendChild(optionEl);
+    optionEl.textContent = aColour;
 
+  }
+  var setColour = function (e) {
+    let paraEl=document.querySelector("p")
+    paraEl.textContent = `You've selected: ${e.target.value}`;
+    paraEl.style.color = e.target.value;
+  };
+  selectEl.addEventListener("click", setColour);
+  
+}
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
 
 listOfColours(colours);
