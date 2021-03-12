272d9f2d521975fa745be6707a55f6542030b6c5
BenyamWorku <benworku@gmail.com>
2021-03-12 19:40:27 +0000

    to-do

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,23 +1,117 @@
-function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
-}
 
-// These are the same todos that currently display in the HTML
-// You will want to remove the ones in the current HTML after you have created them using JavaScript
+// starter to do array
 let todos = [
   { task: "Wash the dishes", completed: false },
   { task: "Do the shopping", completed: false },
 ];
+let ulTag = document.createElement("ul");
+ulTag.classList.add("list-group");
+ulTag.setAttribute("id", "todo-list");
+document.getElementById("content")
+  .querySelector(".container")
+  .querySelector(".row")
+  .querySelector(".col-sm").appendChild(ulTag)
+
+
+
+// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
+
+
+// populating 
+// for (let [i, chore] of todos.entries()) {
+//   console.log(ulTag);
+
+//   let liTag = document.createElement("li");
+//   liTag.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+//   liTag.appendChild(document.createTextNode(chore.task));
+
+//   let spanTag = document.createElement("span");
+//   spanTag.classList.add("badge", "bg-primary", "rounded-pill");
+//   liTag.appendChild(spanTag);
+
+//   let idiomCheckTag = document.createElement("i");
+//   idiomCheckTag.classList.add("fa", "fa-check","spacer");
+//   idiomCheckTag.setAttribute("aria-hidden", "false")
+
+//   let idiomTrashTag = document.createElement("i");
+//   idiomTrashTag.classList.add("fa", "fa-trash","spacer");
+//   idiomTrashTag.setAttribute("aria-hidden", "false");
+
+//   spanTag.appendChild(idiomCheckTag);
+//   spanTag.appendChild(idiomTrashTag);
+
+//   ulTag.appendChild(liTag);
+
+
+// }
+
+function framework() {
+  
+}
+function populateTodoList(todos) {
+  for (let [i, chore] of todos.entries()) {
+    console.log(ulTag);
+
+    let liTag = document.createElement("li");
+    liTag.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+    liTag.appendChild(document.createTextNode(chore.task));
+
+    let spanTag = document.createElement("span");
+    spanTag.classList.add("badge", "bg-primary", "rounded-pill");
+    liTag.appendChild(spanTag);
+
+    let idiomCheckTag = document.createElement("i");
+    idiomCheckTag.classList.add("fa", "fa-check", "spacer");
+    idiomCheckTag.setAttribute("aria-hidden", "false")
+
+    let idiomTrashTag = document.createElement("i");
+    idiomTrashTag.classList.add("fa", "fa-trash", "spacer");
+    idiomTrashTag.setAttribute("aria-hidden", "false");
+
+    spanTag.appendChild(idiomCheckTag);
+    spanTag.appendChild(idiomTrashTag);
+
+    ulTag.appendChild(liTag);
+  }
+
+}
 
 populateTodoList(todos);
 
-// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
+// console.log(ulTag.tagName);
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  let inputEl = document.querySelector("input");
+  let newTask = { task: inputEl.value, completed: false };
+  if (inputEl.value.length > 0) {
+    // console.log(inputEl.value);
+    todos.push(newTask);
+  }
+  // populateTodoList(newTask);
+  inputEl.value = "";
+  console.log(todos);
+  populateTodoList(todos);
+  console.log(todos);
+
 }
+//
+// window.onload = populateTodoList(todos);
+
+
+function removeTodo(event) {
+  if (event.target.classList.contains("fa-trash"))
+    event.target.parentElement.parentElement.remove();
+}
+
+function crossWhenDone(event) {
+  if (event.target.classList.contains("fa-check"))
+    event.target.parentElement.parentElement.classList.toggle("done");
+}
+//event listeners 
+ulTag.addEventListener("click", crossWhenDone);
+ulTag.addEventListener("click", removeTodo);
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
